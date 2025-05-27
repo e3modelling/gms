a21515bffc6281d2a007632c0163702b7b94517b
Fsioutas <fsioutas@hotmail.com>
2025-05-27 14:00:13 +0300

    Update codeCheck.R

--- R/codeCheck.R
+++ R/codeCheck.R
@@ -114,7 +114,7 @@ codeCheck <- function(path = ".",
     # for further information)
     # Remove objects which do not follow the naming conventions from the declarations set as
     # they would otherwise cause problems in what follows
-    tmp <- grep("^[qvsfipoxcmV]{1}[cqv]?(M|[0-9]{2}|)_", gams$declarations[, "names"], invert = TRUE)
+    tmp <- grep("^[qvsfipoxcmV]{1}[cqv]?(M|[0-9]{2}|)", gams$declarations[, "names"], invert = TRUE)
     tmp <- tmp[gams$declarations[tmp, "type"] != "set"] # remove set entries from analysis
     if (length(tmp) > 0) {
       declarationNames <- gams$declarations[tmp, "names"]
